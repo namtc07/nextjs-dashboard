bb374e4c39b34213768f8732b326e02325483995
namtc07 <truongcongnam1207@gmail.com>
2023-12-15 11:59:05 +0700

    fix: create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,16 +17,18 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerID: formData.get('customerId'),
+    customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInCents = amount * 100;
+
+  const amountInCents = Math.round(amount * 100);
+
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId},${amountInCents},${status},${date})
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 
   revalidatePath('/dashboard/invoices');
