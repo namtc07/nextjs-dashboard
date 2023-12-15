38addbd0bd9c1a307566ac34b4aace0b23084caa
namtc07 <truongcongnam1207@gmail.com>
2023-12-15 16:18:03 +0700

    chapter 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,14 +12,8 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
@@ -167,7 +161,6 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
